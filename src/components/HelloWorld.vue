3c23883bfff4f0bc8876cbece2352afd3c58984d
Berkin Akkaya <berkin_akkaya@hotmail.com>
2025-02-13 17:58:32 +0300

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useCounterStore } from "../stores/counter";
+const store = useCounterStore();
+</script>
+
+<template>
+  <h1>Count is {{ store.count }}</h1>
+
+  <h2>Double is {{ store.doubleCount }}</h2>
+
+  <button @click="store.increment()">Increment</button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>

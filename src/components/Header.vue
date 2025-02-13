aa1f99dc1fd90ac347c6d2f14d4ed74836523ef7
Berkin Akkaya <berkin_akkaya@hotmail.com>
2025-02-13 18:46:30 +0300

    prettier, tailwind, header

---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+	<header>
+		<h1>Insider Personal Finance Tracker</h1>
+	</header>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import './styles/style.css';
+import './styles/reset.css';
 
 const app = createApp(App);
 
